61f1eb71251e3283166bfbe52d27cc6f8478665c
Kamran <melikovk@gmail.com>
2019-07-14 23:11:23 -0400

    Streamline training script
    Changes to be committed:
    modified:   train_model.py

--- train_model.py
+++ train_model.py
@@ -1,146 +1,101 @@
-import glob
-import os.path
 import torch
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader, ConcatDataset
-from trainsession import TrainSession, SessionSaver
 import numpy as np
+import torch.optim as optim
+from torch.utils.data import ConcatDataset
+from trainsession import TrainSession, SessionSaver, get_filenames
 import torchvision.transforms as transforms
 import image.cv2transforms as cv2transforms
-from model_zoo import mobilenet_v2, cnn_heads
-from losses import yolo1_loss, yolo2_loss, object_detection_loss
-from model_zoo.vision_models import ObjectDetectionModel, saveboxes
-from image.datasets.yolo import YoloDataset, RandomLoader, labels_to_boxes, get_cell_anchors, SSDDataset
-from image.metrics.localization import precision_recall_f1, precision_recall_meanIOU
+from losses import ObjectDetectionLoss
+from model_zoo.vision_models import ObjectDetectionModel
+from image.datasets.yolo import YoloDataset, RandomLoader, SSDDataset
+from image.metrics.localization import PrecisionRecallMeanIOU
 from model_zoo import catalog
-import argparse
 from functools import partial
+from os.path import expanduser
 
-""" Training and model parameters are defined below """
-
-# Model to train
-MODEL_CLASS = catalog.mobilenet_v2_base_1ch_split_yolo
-# Location of data files
-DATADIR = '~/workspace/fusion_data/Automate/20x/'
-# Imaging chanel to use (i.e. name of the subfolder with images )
-CHANNEL = 'nuclei'
-# Data transformation to apply to images
-DATA_TRANSFORMS = transforms.Compose([
-    cv2transforms.AutoContrast(),
-    cv2transforms.Typecast(np.float32),
-    imgToTensor ])
-# Location to save training session. Set to None to disable session saving
-MODELDIR = '~/workspace/cellfusion/models/flnuc_mobilenet2_split_base_lw5'
-# Location to save log data (loss and accuracy log). Set to None to disable logging
-LOGDIR = '~/workspace/cellfusion/models/flnuc_mobilenet2_split_base_lw5'
-# Set Parameters of trainning Dataset below
-TRAIN_DATASET_CLASS = SSDDataset
-TRAIN_DATASET_PARAMETERS = {
-'win_size': (224, 224),
-'border_size': 32,
-'positive_anchor_threshold': 0.7,
-'background_anchor_threshold': 0.5,
-'denominator': 'union',
-'window_overlap_threshold': .25,
-'grid_size': 32,
-'sample': 'random', # random or grid
-'length': 500, # works only when sample == 'random'
-'seed': None,  # works only when sample == 'random'
-'stride': None, # Works only when sample == 'grid'
-'transforms': data_transforms }
-# Set Parameters of trainning Dataset below
-TEST_DATASET_CLASS = SSDDataset
-TEST_DATASET_PARAMETERS = {
-'win_size': (224, 224),
-'border_size': 32,
-'positive_anchor_threshold': 0.7,
-'background_anchor_threshold': 0.5,
-'denominator': 'union',
-'window_overlap_threshold': .25,
-'grid_size': 32,
-'sample': 'random', # random or grid
-'length': 100, # works only when sample == 'random'
-'seed': None,  # works only when sample == 'random'
-'stride': None, # Works only when sample == 'grid'
-'transforms': data_transforms }
-# Set loss function and it parameters below
-LOSS_FUNCTION = object_detection_loss
-LOSS_PARAMETERS = {
-'reduction': 'mean',
-'confidence_loss': 'crossentropy',
-'size_transform': 'none',
-'localization_weight': 5.0,
-'normalize_per_anchor': True }
-# Set optimizer at default parameter below
-OPTIMIZER = optim.Adam
-OPTIMIZER_DEFAULTS = {
-'lr': 0.01,
-'weight_decay':1e-5}
-# Set accuracy function and its parameters below
-ACCURACY_FUNCTION = precision_recall_meanIOU
-ACCURRACY_PARAMETERS = {
-'iou_thresholds': [0.5, 0.7, 0.9],
-'nms_threshold': 0.8}
-# Set scheduler and scheduler options below
-SCHEDULER = optim.lr_scheduler.CosineAnnealingLR,
-SCHEDULER_PARAMETERS = {
-'T_max': 100 }
-# Set training device
-DEVICE = 'cuda:0'
-# Set train dataloader class and dataloader parameters
-TRAIN_DATALOADER_CLASS = RandomLoader
-TRAIN_DATALOADER_PARAMETERS = {
-'batch_size': 32,
-'shuffle': True,
-'num_workers': 2 }
-# Set test dataloader class and dataloader parameters
-TEST_DATALOADER_CLASS = RandomLoader
-TEST_DATALOADER_PARAMETERS = {
-'batch_size': 32,
-'shuffle': True,
-'num_workers': 2 }
-LEARNING_RATE_MULTIPLIER = 0.1
-NUMBER_OF_TRAINING_CYCLES = 5
-
-def imgToTensor(img):
-    return torch.unsqueeze(torch.from_numpy(img), 0)
-
-def get_filenames(datadir, channel):
-    chanel += '/'
-    with open(datadir+'train.txt') as f:
-        train_names = [(datadir+chanel+name[:-1]+'.tif', datadir+'boxes/'+name[:-1]+'boxes.json')
-                        for name in f.readlines()]
-    with open(datadir+'test.txt') as f:
-        test_names = [(datadir+chanel+name[:-1]+'.tif', datadir+'boxes/'+name[:-1]+'boxes.json')
-                        for name in f.readlines()]
-    return train_names, test_names
-
-def train_model(modelclass, datadir, channel, data_transforms, train_dataset_class,
-    train_dataset_parameters, test_dataset_class, test_dataset_parameters, train_dataloader_class,
-    train_dataloader_parameters, test_dataloader_class, test_dataloader_parameters,
-    loss_function, loss_parameters, optimizer, optimizer_defaults, accuracy_function,
-    accuracy_parameters, scheduler, scheduler_parameters, modeldir, logdir, device,
-    lr_multiplier, n_cycles):
-    """ Procedure that trains mobilenet_v2 based nucleus recognition models
+def train_model(datadir, channel, dataset_type, train_batch, test_batch, modeldir, logdir,
+    init_lr, cycle_length, device, lr_multiplier, num_cycles):
+    """ Procedure that trains object recognition models
     Parameters:
         datadir: folder with train and test data
         device:  device to use for training
     """
+    model = catalog.MobilenetBase1chSplitHead()
+
     train_names, test_names = get_filenames(datadir, channel)
-    model = modelclass()
-    trainDataset = ConcatDataset([train_dataset_class(*names,
+
+    data_transforms = transforms.Compose([cv2transforms.AutoContrast()])
+
+    train_dataset_parameters = {
+    'win_size': (224, 224),
+    'border_size': 32,
+    'window_overlap_threshold': .25,
+    'grid_size': model.features.grid_size,
+    'length': 500}
+    if dataset_type == 'SSD':
+        train_dataset_parameters['positive_anchor_threshold'] = 0.7
+        train_dataset_parameters['background_anchor_threshold'] = 0.5
+    elif dataset_type == 'YOLO':
+        train_dataset_parameters['anchor_ignore_threshold'] = 0.5
+    else:
+        raise ValueError('dataset_type should be either SSD or YOLO')
+    test_dataset_parameters = train_dataset_parameters.copy()
+    test_dataset_parameters['length'] = 100
+
+    if dataset_type == 'SSD':
+        dataset_class = SSDDataset
+    else:
+        dataset_class = YoloDataset
+
+    trainDataset = ConcatDataset([dataset_class(*names,
         cell_anchors = model.cell_anchors,
         transforms = data_transforms,
         **train_dataset_parameters) for names in train_names])
-    testDataset = ConcatDataset([test_dataset_class(*names,
+
+    testDataset = ConcatDataset([dataset_class(*names,
         cell_anchors = model.cell_anchors,
         transforms = data_transforms,
         **test_dataset_parameters) for names in test_names])
-    trainDataLoader = train_dataloader_class(trainDataset, **train_dataloader_parameters}
-    testDataLoader = test_dataloader_class(ttestDataset, **test_dataloader_parameters)
-    lossfunc = partial(loss_function, **loss_parameters)
-    accuracy = partial(accuracy_function, **accuracy_parameters)
-    saver = None if modeldir is None else SessionSaver(modeldir)
+
+    train_dataloader_parameters = {
+    'batch_size': train_batch,
+    'shuffle': True,
+    'num_workers': 2 }
+
+    test_dataloader_parameters = {
+    'batch_size': train_batch,
+    'shuffle': True,
+    'num_workers': 2 }
+
+    trainDataLoader = RandomLoader(trainDataset, **train_dataloader_parameters)
+    testDataLoader = RandomLoader(testDataset, **test_dataloader_parameters)
+
+    loss_parameters = {
+    'confidence_loss': 'crossentropy',
+    'size_transform': 'none',
+    'localization_weight': 5.0,
+    'normalize_per_anchor': True }
+
+    lossfunc = ObjectDetectionLoss(**loss_parameters)
+
+    accuracy_parameters = {
+    'iou_thresholds': [0.5, 0.7, 0.9],
+    'nms_threshold': 0.8}
+
+    accuracy = PrecisionRecallMeanIOU(**accuracy_parameters)
+
+    saver = SessionSaver(modeldir)
+
+    optimizer = optim.Adam
+
+    optimizer_defaults = {
+    'lr': init_lr,
+    'weight_decay':1e-5}
+
+    scheduler = optim.lr_scheduler.CosineAnnealingLR
+    scheduler_parameters = {
+    'T_max': cycle_length }
+
     session = TrainSession(model,
                            lossfunc,
                            optimizer,
@@ -152,31 +107,22 @@ def train_model(modelclass, datadir, channel, data_transforms, train_dataset_cla
                            scheduler_params = scheduler_parameters,
                            saver = saver,
                            device = torch.device(device))
-    for i in range(n_cycles):
-        session.train(trainDataLoader, testDataLoader, t_max)
+    for i in range(num_cycles):
+        session.train(trainDataLoader, testDataLoader, cycle_length)
         session.update_lr(lr_multiplier)
 
 if __name__ == "__main__":
-    train_model(modelclass = MODEL_CLASS,
-        datadir = DATADIR,
-        channel = CHANNEL,
-        data_transforms = DATA_TRANSFORMS,
-        train_dataset_class = TRAIN_DATASET_CLASS,
-        train_dataset_parameters = TRAIN_DATASET_PARAMETERS,
-        test_dataset_class = TEST_DATASET_CLASS,
-        test_dataset_parameters = TEST_DATASET_PARAMETERS,
-        train_dataloader_class = TRAIN_DATASET_CLASS,
-        train_dataset_parameters = TRAIN_DATALOADER_CLASS,
-        test_dataloader_class = TEST_DATALOADER_CLASS,
-        test_dataloader_parameters = TEST_DATALOADER_PARAMETERS,
-        loss_function = LOSS_FUNCTION,
-        loss_parameters = LOSS_PARAMETERS,
-        accuracy_function = ACCURACY_FUNCTION,
-        accuracy_parameters = ACCURRACY_PARAMETERS,
-        scheduler = SCHEDULER,
-        scheduler_parameters = SCHEDULER_PARAMETERS,
-        modeldir = MODELDIR,
-        logdir = LOGDIR,
-        device = DEVICE,
-        lr_multiplier = LEARNING_RATE_MULTIPLIER,
-        n_cycle = NUMBER_OF_TRAINING_CYCLES)
+    parameters= {
+    'datadir': expanduser('~/fast/Automate/20x/'),
+    'channel': 'nuclei',
+    'dataset_type': 'SSD',
+    'train_batch': 32,
+    'test_batch': 32,
+    'modeldir': 'models/mobilenet2_test',
+    'logdir': 'runs/mobilenet2_test_',
+    'init_lr': 0.01,
+    'cycle_length': 100,
+    'device': 'cuda:0',
+    'lr_multiplier': 0.1,
+    'num_cycles': 5}
+    train_model(**parameters)
