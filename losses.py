118e54658a41ea35d264a8ee0a7f0e70c8f29b73
Kamran <melikovk@gmail.com>
2019-07-19 17:34:01 -0400

    Add focal_loss function. Optimize ObjectDetectionLoss
    Changes to be committed:
    modified:   losses.py

--- losses.py
+++ losses.py
@@ -24,10 +24,10 @@ class ObjectDetectionLoss:
         {'loss': total_loss, 'confidence_loss':confidence_loss, 'localization_loss':localization_loss}
     """
     def __init__(self, reduction='mean', confidence_loss = 'crossentropy', size_transform = 'none',
-        localization_weight = 1, normalize_per_anchor = True):
+        localization_weight = 1, normalize_per_anchor = True, **kwargs):
         assert reduction == 'mean' or reduction == 'sum', \
             "reduction should be 'mean' or 'sum'"
-        assert confidence_loss == 'crossentropy' or confidence_loss == 'mse', \
+        assert confidence_loss in ['crossentropy', 'mse', 'focal_loss'], \
             "confidence_loss should be 'crossentropy' or 'mse'"
         assert size_transform in {'none', 'log', 'sqrt'}, \
             "size_transform should be 'none', 'log' or 'sqrt'"
@@ -36,6 +36,7 @@ class ObjectDetectionLoss:
         self.size_transform = size_transform
         self.localization_weight = localization_weight
         self.normalize_per_anchor = normalize_per_anchor
+        self.kwargs = kwargs
 
     def __call__(self, predict, target):
         assert predict.shape == target.shape, \
@@ -43,19 +44,24 @@ class ObjectDetectionLoss:
         batch_size, _, w, h = predict.shape
         predict = predict.reshape(batch_size, 5, -1, w, h)
         target = target.reshape(batch_size, 5, -1, w, h)
+        # Calculate object confidence loss
         object_mask = target[:,0,...] > -0.5
         if self.confidence_loss == 'crossentropy':
-            loss_conf = F.binary_cross_entropy_with_logits(torch.masked_select(predict[:,0,...], object_mask), torch.masked_select(target[:,0,...], object_mask), reduction='sum')
-        else :
-            loss_conf = F.mse_loss(torch.sigmoid(torch.masked_select(predict[:,0,...], object_mask)), torch.masked_select(target[:,0,...], object_mask), reduction='sum')
-        box_mask = target[:,0:1,...] > 0.5
-        loss_box = F.mse_loss(torch.masked_select(predict[:,1:3,...], box_mask), torch.masked_select(target[:,1:3,...], box_mask), reduction='sum')
-        if self.size_transform == 'log':
-            loss_box += F.mse_loss(torch.masked_select(predict[:,3:,...], box_mask).log(), torch.masked_select(target[:,3:,...], box_mask).log(), reduction='sum')
-        elif self.size_transform == 'sqrt':
-            loss_box += F.mse_loss(torch.masked_select(predict[:,3:,...], box_mask).sqrt(), torch.masked_select(target[:,3:,...], box_mask).sqrt(), reduction='sum')
+            loss_conf = torch.masked_select(F.binary_cross_entropy_with_logits(predict[:,0,...], target[:,0,...], reduction='none'), object_mask).sum()
+        elif self.confidence_loss == 'mse':
+            loss_conf = torch.masked_select(F.mse_loss(torch.sigmoid(predict[:,0,...]), target[:,0,...], reduction='none'), object_mask).sum()
         else:
-            loss_box += F.mse_loss(torch.masked_select(predict[:,3:,...], box_mask), torch.masked_select(target[:,3:,...], box_mask), reduction='sum')
+            loss_conf = torch.masked_select(_focal_loss(predict[:,0,...], target[:,0,...], **self.kwargs), object_mask).sum()
+        # Calculate loaclization loss
+        box_mask = target[:,0:1,...] > 0.5
+        loss_box = torch.masked_select(F.mse_loss(predict[:,1:3,...], target[:,1:3,...], reduction='none'), box_mask).sum()
+        # Transform box sizes if requested
+        if self.size_transform == 'log':
+            loss_box += torch.masked_select(F.mse_loss(predict[:,3:,...].log(), target[:,3:,...].log(), reduction='none'), box_mask).sum()
+        elif self.size_transform == 'sqrt':
+            loss_box += torch.masked_select(F.mse_loss(predict[:,3:,...].sqrt(), target[:,3:,...].sqrt(), reduction='none'), box_mask).sum()
+        else:
+            loss_box += torch.masked_select(F.mse_loss(predict[:,3:,...], target[:,3:,...], reduction='none'), box_mask).sum()
         if self.reduction == 'mean':
             loss_conf, loss_box = loss_conf/target.shape[0], loss_box/target.shape[0]
         if self.normalize_per_anchor:
@@ -78,94 +84,22 @@ class ObjectDetectionLoss:
         self.localization_weight = state['localization_weight']
         self.normalize_per_anchor = state['normalize_per_anchor']
 
-def object_detection_loss(predict, target, reduction='mean', confidence_loss = 'crossentropy',
-    size_transform = 'log', localization_weight = 1, normalize_per_anchor = True):
-    """ Loss function for object detection for dense grid of predictions such
-    as in YOLO and SSD detectors. Predictions are assumed to be groupped in the following way :
-    objectness score's for all anchors, x's for all anchors, y's for all anchors
-    followed by w's and h's for all anchors. Function ignores target anchors
-    with negative scores, allowing setting anchor boxes in the target to -1 to be ignored
-    in calculation of loss (both objectness and box coordinates)
-    Parameters:
-        predict: (batch_size, 5xNAnchors, h, w) 4D Tensor of predictions
-        target:  (batch_size, 5xNAnchors, h, w) 4D Tensor of ground truth boxes
-        reduction: {'mean'|'sum'} sum or average over the batch
-        confidence_loss: {'crossentropy'|'mse'}
-        confidence_output: {'logits'|'probability'}
-        size_transform: {'log'|'sqrt'|'none'}
-        localization_weight: Factor to multiply localization loss
-                             before summing with confidence loss
-                             default = 1
-    Returns:
-        {'loss': total_loss, 'confidence_loss':confidence_loss, 'localization_loss':localization_loss}
+def _focal_loss(predict, target, alpha=.25, gamma=2.0, reduction='none'):
+    """ Loss function to calculate Focal Loss for object detection confidence
+        FL(p_t) = -alpha_t(1-p_t)**gamma*log(p_t), where
+        p_t = p if y == 1 else (1-p), p is predicted probabilty, y is target probability
+        a_t = a if y == 1 else (1-a), a is a parameter
     """
-    assert predict.shape == target.shape, \
-        "prediction and target tensors should have the same shape"
-    batch_size, _, w, h = predict.shape
-    predict = predict.reshape(batch_size, 5, -1, w, h)
-    target = target.reshape(batch_size, 5, -1, w, h)
-    object_mask = target[:,0,...] > -0.5
-    if confidence_loss == 'crossentropy':
-        loss_conf = F.binary_cross_entropy_with_logits(torch.masked_select(predict[:,0,...], object_mask), torch.masked_select(target[:,0,...], object_mask), reduction='sum')
-    elif confidence_loss == 'mse':
-        loss_conf = F.mse_loss(torch.sigmoid(torch.masked_select(predict[:,0,...], object_mask)), torch.masked_select(target[:,0,...], object_mask), reduction='sum')
-    else:
-        raise ValueError("confidence_loss should be 'crossentropy' or 'mse'")
-    box_mask = target[:,0:1,...] > 0.5
-    loss_box = F.mse_loss(torch.masked_select(predict[:,1:3,...], box_mask), torch.masked_select(target[:,1:3,...], box_mask), reduction='sum')
-    if size_transform == 'log':
-        loss_box += F.mse_loss(torch.masked_select(predict[:,3:,...], box_mask).log(), torch.masked_select(target[:,3:,...], box_mask).log(), reduction='sum')
-    elif size_transform == 'sqrt':
-        loss_box += F.mse_loss(torch.masked_select(predict[:,3:,...], box_mask).sqrt(), torch.masked_select(target[:,3:,...], box_mask).sqrt(), reduction='sum')
-    elif size_transform == 'none':
-        loss_box += F.mse_loss(torch.masked_select(predict[:,3:,...], box_mask), torch.masked_select(target[:,3:,...], box_mask), reduction='sum')
-    else:
-        raise ValueError("size_transform should be 'log', 'sqrt' or 'none'")
+    device = predict.device
+    p = torch.sigmoid(predict)
+    loss = F.binary_cross_entropy_with_logits(predict, target, reduction='none')
+    loss = loss * torch.pow(torch.where(target > 0.5, p, 1-p), gamma)
+    loss = loss * torch.where(target > 0.5, torch.tensor(alpha).to(device), torch.tensor(1-alpha).to(device))
+    if reduction == 'sum':
+        loss = loss.sum()
     if reduction == 'mean':
-        loss_conf, loss_box = loss_conf/target.shape[0], loss_box/target.shape[0]
-    elif reduction != 'sum':
-        raise ValueError("reduction should be 'mean' or 'sum'")
-    if normalize_per_anchor:
-        loss_conf = loss_conf/target.shape[2]
-    loss = loss_conf + localization_weight*loss_box
-    return {'loss':loss, 'confidence_loss': loss_conf, 'localization_loss':localization_weight*loss_box}
-
-def object_detection_loss_fast(predict, target, reduction='mean', confidence_loss = 'crossentropy',
-    size_transform = 'log', localization_weight = 1, eps = 1e-5, normalize_per_anchor = True):
-    """ This function is equivalent to object_detection_loss but uses multiplication by 0 for masking
-    instead of torch.masked_select which is significantly slower.
-    """
-    assert predict.shape == target.shape, \
-        "prediction and target tensors should have the same shape"
-    eps = torch.tensor(eps).to(target.device)
-    batch_size, _, w, h = predict.shape
-    predict = predict.reshape(batch_size, 5, -1, w, h)
-    target = target.reshape(batch_size, 5, -1, w, h)
-    object_mask = ((target[:,0,...]) > -0.5).to(torch.float)
-    if confidence_loss == 'crossentropy':
-        loss_conf = F.binary_cross_entropy_with_logits(predict[:,0,...]*object_mask, target[:,0,...]*object_mask, reduction='sum')
-    elif confidence_loss == 'mse':
-        loss_conf = F.mse_loss(torch.sigmoid(predict[:,0,...])*object_mask, target[:,0,...]*object_mask, reduction='sum')
-    else:
-        raise ValueError("confidence_loss should be 'crossentropy' or 'mse'")
-    box_mask = (target[:,0:1,...] > 0).to(torch.float)
-    loss_box = F.mse_loss(predict[:,1:3,...]*box_mask, target[:,1:3,...]*box_mask, reduction='sum')
-    if size_transform == 'log':
-        loss_box += F.mse_loss(predict[:,3:,...].log()*box_mask, target[:,3:,...].max(eps).log()*box_mask, reduction='sum')
-    elif size_transform == 'sqrt':
-        loss_box += F.mse_loss(predict[:,3:,...].sqrt()*box_mask, target[:,3:,...].sqrt()*box_mask, reduction='sum')
-    elif size_transform == 'none':
-        loss_box += F.mse_loss(predict[:,3:,...]*box_mask, target[:,3:,...]*box_mask, reduction='sum')
-    else:
-        raise ValueError("size_transform should be 'log', 'sqrt' or 'none'")
-    if reduction == 'mean':
-        loss_conf, loss_box = loss_conf/target.shape[0], loss_box/target.shape[0]
-    elif reduction != 'sum':
-        raise ValueError("reduction should be 'mean' or 'sum'")
-    if normalize_per_anchor:
-        loss_conf = loss_conf/target.shape[2]
-    loss = loss_conf + localization_weight*loss_box
-    return {'loss':loss, 'confidence_loss': loss_conf, 'localization_loss':localization_weight*loss_box}
+        loss = loss / loss.mean()
+    return loss
 
 def yolo1_loss(predict, target, reduction='mean', localization_weight = 1):
     return object_detection_loss(predict, target, reduction=reduction, confidence_loss = 'mse',
