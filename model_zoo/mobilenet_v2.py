e789e92d5f82e9f15205bb0a3b1ec81c079db462
Kamran <melikovk@gmail.com>
2019-09-29 18:48:31 -0400

    Modify MobileNet to optionally output multiple feature maps
    Changes to be committed:
    modified:   model_zoo/mobilenet_v2.py

--- model_zoo/mobilenet_v2.py
+++ model_zoo/mobilenet_v2.py
@@ -44,6 +44,7 @@ class Block(nn.Module):
 
     def __init__(self, in_channels, out_channels, repeats = 1, stride = 2, expansion = 6, bn_args={'momentum':0.01}):
         super().__init__()
+        self.stride = stride
         self.linear_btlneck = Bottleneck(in_channels, out_channels, stride, expansion, bn_args)
         self.linear_activation = nn.ReLU6()
         for i in range(1, repeats):
@@ -54,16 +55,41 @@ class Block(nn.Module):
             x = m(x)
         return x
 
+# class MobileNetV2(nn.Module):
+#
+#     def __init__(self, in_channels, out_channels=(32,16,24,32,64,96,160,320), repeats=(1,2,3,4,3,3,1), strides=(1,2,2,2,1,2,1), expansions=(1,6,6,6,6,6,6), bn_args={'momentum':0.01}):
+#         super().__init__()
+#         self.grid_size = np.prod(strides)*2
+#         self.conv_init = nn.Conv2d(in_channels, out_channels[0], 3, stride=2, padding=1)
+#         self.bn_init = nn.BatchNorm2d(out_channels[0], **bn_args)
+#         self.activation_init = nn.ReLU6()
+#         for i in range(len(expansions)):
+#             self.add_module(f'block_{i+1}',Block(out_channels[i], out_channels[i+1], repeats[i], strides[i], expansions[i], bn_args))
+#         # Parameter initialization
+#         init.kaiming_uniform_(self.conv_init.weight, mode='fan_in', nonlinearity='relu')
+#         init.zeros_(self.conv_init.bias)
+#         init.ones_(self.bn_init.weight)
+#         init.zeros_(self.bn_init.bias)
+#
+#     def forward(self,x):
+#         for m in self._modules.values():
+#             x = m(x)
+#         return x
+
 class MobileNetV2(nn.Module):
 
-    def __init__(self, in_channels, out_channels=(32,16,24,32,64,96,160,320), repeats=(1,2,3,4,3,3,1), strides=(1,2,2,2,1,2,1), expansions=(1,6,6,6,6,6,6), bn_args={'momentum':0.01}):
+    def __init__(self, in_channels, features_num = [-1], out_channels=(32,16,24,32,64,96,160,320),
+        repeats=(1,2,3,4,3,3,1), strides=(1,2,2,2,1,2,1), expansions=(1,6,6,6,6,6,6), bn_args={'momentum':0.01}):
+
         super().__init__()
         self.grid_size = np.prod(strides)*2
+        self.features_num = features_num
         self.conv_init = nn.Conv2d(in_channels, out_channels[0], 3, stride=2, padding=1)
         self.bn_init = nn.BatchNorm2d(out_channels[0], **bn_args)
         self.activation_init = nn.ReLU6()
+        self.blocks = nn.ModuleList()
         for i in range(len(expansions)):
-            self.add_module(f'block_{i+1}',Block(out_channels[i], out_channels[i+1], repeats[i], strides[i], expansions[i], bn_args))
+            self.blocks.append(Block(out_channels[i], out_channels[i+1], repeats[i], strides[i], expansions[i], bn_args))
         # Parameter initialization
         init.kaiming_uniform_(self.conv_init.weight, mode='fan_in', nonlinearity='relu')
         init.zeros_(self.conv_init.bias)
@@ -71,6 +97,22 @@ class MobileNetV2(nn.Module):
         init.zeros_(self.bn_init.bias)
 
     def forward(self,x):
-        for m in self._modules.values():
-            x = m(x)
-        return x
+        feature_maps = []
+        x = self.activation_init(self.bn_init(self.conv_init(x)))
+        feature_maps.append(x)
+        for block in self.blocks:
+            x = block(x)
+            if block.stride > 1:
+                feature_maps.append(x)
+            else:
+                feature_maps[-1] = x
+        return feature_maps[-self.features_num:][-1::-1]
+
+def convert_parameters(parameters):
+    new_params = OrderedDict()
+    def repl_func(match):
+        block_num = match.groups()[1]
+        return f'blocks.{int(block_num)-1}'
+    for pname, p in parameters.items():
+        new_params[re.sub("(block_)(\d)", repl_func, pname)] = p
+    return new_params
