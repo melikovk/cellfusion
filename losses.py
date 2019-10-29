cd9a6eb1ecf434a7344df596051438c27cfd8798
Kamran <melikovk@gmail.com>
2019-10-29 14:01:57 -0400

    Update loss to take single tensor with objectness, coordinates and class
    Changes to be committed:
    modified:   losses.py

--- losses.py
+++ losses.py
@@ -23,7 +23,7 @@ class ObjectDetectionLoss:
     Returns:
         {'loss': total_loss, 'confidence_loss':confidence_loss, 'localization_loss':localization_loss}
     """
-    def __init__(self, reduction='mean', confidence_loss = 'crossentropy', size_transform = 'none',
+    def __init__(self, clsnums = [], reduction='mean', confidence_loss = 'crossentropy', size_transform = 'none',
         localization_weight = 1., classification_weight = 1., normalize_per_anchor = True, normalize_per_cell = True, **kwargs):
         assert reduction == 'mean' or reduction == 'sum', \
             "reduction should be 'mean' or 'sum'"
@@ -31,6 +31,7 @@ class ObjectDetectionLoss:
             "confidence_loss should be 'crossentropy' or 'mse'"
         assert size_transform in {'none', 'log', 'sqrt'}, \
             "size_transform should be 'none', 'log' or 'sqrt'"
+        self.clsnums = clsnums
         self.reduction = reduction
         self.confidence_loss = confidence_loss
         self.size_transform = size_transform
@@ -41,71 +42,70 @@ class ObjectDetectionLoss:
         self.kwargs = kwargs
 
     def __call__(self, predict, target):
-        if isinstance(predict, list):
-            assert isinstance(target, list) and len(predict) == len(target), \
-                "for class predictions lists of the same size are expected {} {}".format(predict, target)
-            add_class_loss = True
-            predict_obj = predict[0]
-            target_obj = target[0]
-        else:
-            add_class_loss = False
-            predict_obj = predict
-            target_obj = target
-        assert predict_obj.shape == target_obj.shape, \
-            "prediction and target tensors should have the same shape"
-        batch_size, _, w, h = predict_obj.shape
-        predict_obj = predict_obj.reshape(batch_size, 5, -1, w, h)
-        target_obj = target_obj.reshape(batch_size, 5, -1, w, h)
+        batch_size, c, w, h = target.shape
+        n_anchors = c // (5+len(self.clsnums))
         # Calculate object confidence loss
-        object_mask = target_obj[:,0,...] > -0.5
+        predict_box = predict[:,-5*n_anchors:,...].reshape(batch_size,5,n_anchors,w,h)
+        target_box = target[:,-5*n_anchors:,...].reshape(batch_size,5,n_anchors,w,h)
+        obj_mask = target_box[:,0,...] > -0.5
         if self.confidence_loss == 'crossentropy':
-            loss_conf = torch.masked_select(F.binary_cross_entropy_with_logits(predict_obj[:,0,...], target_obj[:,0,...], reduction='none'), object_mask).sum()
+            loss_conf = torch.masked_select(F.binary_cross_entropy_with_logits(predict_box[:,0,...],
+                target_box[:,0,...], reduction='none'), obj_mask).sum()
         elif self.confidence_loss == 'mse':
-            loss_conf = torch.masked_select(F.mse_loss(torch.sigmoid(predict_obj[:,0,...]), target_obj[:,0,...], reduction='none'), object_mask).sum()
+            loss_conf = torch.masked_select(F.mse_loss(torch.sigmoid(predict_box[:,0,...]),
+                target_box[:,0,...], reduction='none'), obj_mask).sum()
         elif self.confidence_loss == 'focal_loss':
-            loss_conf = torch.masked_select(_focal_loss(predict_obj[:,0,...], target_obj[:,0,...], **self.kwargs), object_mask).sum()
+            loss_conf = torch.masked_select(_focal_loss(predict_box[:,0,...],
+                target_box[:,0,...], **self.kwargs), obj_mask).sum()
         else:
-            loss_conf = torch.masked_select(_focal_loss_star(predict_obj[:,0,...], target_obj[:,0,...], **self.kwargs), object_mask).sum()
+            loss_conf = torch.masked_select(_focal_loss_star(predict_box[:,0,...],
+                target_box[:,0,...], **self.kwargs), box_mask).sum()
         # Calculate localization loss
-        box_mask = target_obj[:,0:1,...] > 0.5
-        loss_box = torch.masked_select(F.mse_loss(predict_obj[:,1:3,...], target_obj[:,1:3,...], reduction='none'), box_mask).sum()
+        box_mask = target_box[:,0:1,...] > 0.5
+        loss_box = torch.masked_select(F.mse_loss(predict_box[:,1:3,...],
+            target_box[:,1:3,...], reduction='none'), box_mask).sum()
         # Transform box sizes if requested
         if self.size_transform == 'log':
-            loss_box += F.mse_loss(torch.masked_select(predict_obj[:,3:,...], box_mask).log(), torch.masked_select(target_obj[:,3:,...], box_mask).log(), reduction='sum')
+            loss_box += F.mse_loss(torch.masked_select(predict_box[:,-2:,...], box_mask).log(),
+                torch.masked_select(target_box[:,-2:,...], box_mask).log(), reduction='sum')
         elif self.size_transform == 'sqrt':
-            loss_box += torch.masked_select(F.mse_loss(predict_obj[:,3:,...].sqrt(), target_obj[:,3:,...].sqrt(), reduction='none'), box_mask).sum()
+            loss_box += torch.masked_select(F.mse_loss(predict_box[:,-2:,...].sqrt(),
+                target_box[:,-2:,...].sqrt(), reduction='none'), box_mask).sum()
         else:
-            loss_box += torch.masked_select(F.mse_loss(predict_obj[:,3:,...], target_obj[:,3:,...], reduction='none'), box_mask).sum()
+            loss_box += torch.masked_select(F.mse_loss(predict_box[:,-2:,...],
+                target_box[:,-2:,...], reduction='none'), box_mask).sum()
         # Calculate classification loss if needed
-        if add_class_loss:
-            loss_class = F.cross_entropy(predict[1].reshape((target[1].shape[0],-1)+target[1].shape[1:]),target[1],reduction='sum', ignore_index=-1)
-        else:
-            loss_class = torch.tensor(0, dtype=torch.float32, requires_grad=True, device = predict_obj.device)
+        loss_class = []
+        ip = jp = 0
+        it = jt = 0
+        for cls_num in self.clsnums:
+            ip, jp = jp, jp+cls_num*n_anchors
+            it, jt = jt, jt+n_anchors
+            loss_class.append(F.cross_entropy(predict[:,ip:jp,...].reshape((batch_size,cls_num,n_anchors,w,h)),
+                target[:,it:jt,...].type(torch.long),reduction='sum', ignore_index=-1))
         # Calculate reductions
         if self.reduction == 'mean':
-            loss_conf = loss_conf/target_obj.shape[0]
-            loss_box = loss_box/target_obj.shape[0]
-            loss_class = loss_class/target_obj.shape[0]
+            loss_conf = loss_conf/batch_size
+            loss_box = loss_box/batch_size
+            loss_class = [l/batch_size for l in loss_class]
         if self.normalize_per_anchor:
-            loss_conf = loss_conf/target_obj.shape[2]
+            loss_conf = loss_conf/n_anchors
         if self.normalize_per_cell:
-            loss_conf = loss_conf/(target_obj.shape[-1]*target_obj.shape[-2])
-            loss_box = loss_box/(target_obj.shape[-1]*target_obj.shape[-2])
-            loss_class = loss_class/(target_obj.shape[-1]*target_obj.shape[-2])
-        loss = loss_conf + self.localization_weight*loss_box + self.classification_weight*loss_class
-        if add_class_loss:
-            loss_dict = {'loss':loss,
-                         'confidence_loss': loss_conf,
-                         'localization_loss':self.localization_weight*loss_box,
-                         'classification_loss':self.classification_weight*loss_class}
-        else:
-            loss_dict = {'loss':loss,
-                         'confidence_loss': loss_conf,
-                         'localization_loss':self.localization_weight*loss_box}
+            s = w*h
+            loss_conf = loss_conf/s
+            loss_box = loss_box/s
+            loss_class = [l/s for l in loss_class]
+        loss = loss_conf + self.localization_weight*loss_box + self.classification_weight*sum(loss_class)
+        loss_dict = {'loss':loss,
+                     'confidence_loss': loss_conf,
+                     'localization_loss':self.localization_weight*loss_box}
+        for i, c_loss in enumerate(loss_class):
+            loss_dict[f'classification_loss_{i}'] = c_loss
         return loss_dict
 
     def state_dict(self):
         state = {'reduction':self.reduction,
+            'clsnums': self.clsnums,
             'confidence_loss': self.confidence_loss,
             'size_transform':self.size_transform,
             'localization_weight':self.localization_weight,
@@ -114,6 +114,7 @@ class ObjectDetectionLoss:
         return state
 
     def load_state_dict(self, state):
+        self.clsnums = state['clsnums']
         self.reduction = state['reduction']
         self.confidence_loss = state['confidence_loss']
         self.size_transform = state['size_transform']
